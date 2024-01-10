e2c8be12a3c532c23e2e1fe54bf30b58abdeee49
FrancoLedesma <francoledesma100@gmail.com>
2024-01-10 14:17:33 -0300

    Testing Nest JS Controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,50 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('greetings')
+  getGreetings(): string {
+    return this.appService.getGreetings();
+  }
+
+  @Get('names/:name')
+  getNames(@Param() params: { name: string }): string {
+    return this.appService.getNames(params.name);
+  }
+
+  // This could be done also in this way
+
+  // getNames(@Param() name: string): string {
+  // return this.appService.getNames(name);
+  // }
+
+  @Get('categories/:id/products/:productId')
+  getCategories(
+    // This also could be done in this 2 ways
+
+    // @Param('id') id: number,
+    // @Param('productId') productId: number,
+    @Param() params: { id: number; productId: number },
+  ): string {
+    // return this.appService.getCategories(id, productId);
+    return this.appService.getCategories(params.id, params.productId);
+  }
+
+  @Get('pagination')
+  getPagination(@Query() params: { limit: number; offset: number }): string {
+    const { limit, offset } = params;
+
+    return this.appService.getPagination(limit, offset);
+  }
+
+  // You can also predefine some queries
+
+  // getPagination(
+  //    @Query('limit') limit = 100, -- This will infer the type number on top of the predefined value
+  //    @Query('offset') offset: number, Here i don't predefine a value, but i type it
+  // ): string {
+  //  const { limit, offset } = params;
+  //
+  //  return this.appService.getPagination(limit, offset);
+  // }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,20 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getGreetings(): string {
+    return 'Greetings from Argentina!';
+  }
+
+  getNames(name: string): string {
+    return `My name is ${name}`;
+  }
+
+  getCategories(id: number, productId: number): string {
+    return `This is the category number: ${id}, and the product number: ${productId}`;
+  }
+
+  getPagination(limit: number, offset: number): string {
+    return `This is the limit: ${limit}, and this is the offset: ${offset}`;
+  }
 }
